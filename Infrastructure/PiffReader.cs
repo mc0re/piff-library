cba5ffbafa79659e64ea39f90c97497c23aed075
mc0re <mnikitin@hotmail.com>
2020-03-21 01:44:25 +0100

    Working on avcC.

---
+++ Infrastructure/PiffReader.cs
@@ -0,0 +1,15 @@
+﻿namespace PiffLibrary
+{
+    internal class PiffReader
+    {
+        internal static int GetInt32(byte[] bytes, int offset)
+        {
+            var res = (bytes[offset] << 24) +
+                      (bytes[offset + 1] << 16) +
+                      (bytes[offset + 2] << 8) +
+                      bytes[offset + 3];
+
+            return res;
+        }
+    }
+}
